0550a628547b83647380d9fb8affb06bcd576735
shun <elsgroup0157@gmail.com>
2018-02-15 09:44:50 +0900

    added resource

---
+++ titanic.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+
+df = pd.read_csv("data/train.csv").replace("male", 0).replace("female", 1)
+df["Age"].fillna(df.Age.median(), inplace=True)
+
+split_data = []
+for survived in [0, 1]:
+    split_data.append(df[df.Survived == survived])
+
+temp = [i["Pclass"].dropna() for i in split_data]
+plt.hist(temp, histtype="barstacked", bins=3)
+plt.show()
+
+temp = [i["Age"].dropna() for i in split_data]
+plt.hist(temp, histtype="barstacked", bins=16)
+plt.show()
+
+df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+df2 = df.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
+
+print(df2.dtypes)
+
+train_data = df2.values
+xs = train_data[:, 2:]  # Pclass以降の変数
+y = train_data[:, 1]  # 正解データ
+
+forest = RandomForestClassifier(n_estimators=100)
+
+# 学習
+forest = forest.fit(xs, y)
+
+test_df = pd.read_csv("data/test.csv").replace("male", 0).replace("female", 1)
+# 欠損値の補完
+test_df["Age"].fillna(df.Age.median(), inplace=True)
+test_df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+test_df2 = test_df.drop(["Name", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"], axis=1)
+
+test_data = test_df2.values
+xs_test = test_data[:, 1:]
+output = forest.predict(xs_test)
+
+print(len(test_data[:, 0]), len(output))
+zip_data = zip(test_data[:, 0].astype(int), output.astype(int))
+predict_data = list(zip_data)
+
+import csv
+
+with open("predict_result_data.csv", "w") as f:
+    writer = csv.writer(f, lineterminator='\n')
+    writer.writerow(["PassengerId", "Survived"])
+    for pid, survived in zip(test_data[:, 0].astype(int), output.astype(int)):
+        writer.writerow([pid, survived])
